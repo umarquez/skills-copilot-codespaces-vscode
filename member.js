fd99c235716bc99209fb91d91cd15ec119877f0a
Uriel Márquez <um@live.com.mx>
2025-01-30 18:59:07 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    // console.log('skillsMember');
+    return {
+        name: 'skillsMember',
+        data: 'skillsMember',
+        templateUrl: 'app/components/member/skillsMember.html',
+        controller: 'skillsMemberCtrl',
+        controllerAs: 'skillsMemberCtrl',
+        bindToController: true
+    };
+}
